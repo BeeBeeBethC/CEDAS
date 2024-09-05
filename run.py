20c35721f35cc62adff54a548c688ef330535208
Bethan Cottell <cottellbc@gmail.com>
2024-09-05 12:11:41 +0000

    checking for valid data, if not valid throws errors and asks user to repeat data input

--- run.py
+++ run.py
@@ -28,19 +28,39 @@ def get_sales_figures():
     set to be valid, it is seven numerical values that are 
     required for input) 
     """
-# while True:
-    print("Please enter most recent sales figures.")
-    print("Data input should consist of seven numbers separated by commas.")
-    print("Example data: 1,2,3,4,5,6,7\n")
+    while True:
+        print("Please enter most recent sales figures.")
+        print("Data input should consist of seven numbers separated by commas.")
+        print("Example data: 1,2,3,4,5,6,7\n")
 
-    figure_str = input("Enter your sales figures here: \n") 
+        figure_str = input("Enter your sales figures here: \n") 
 
-    sales_figures = figure_str.split(',')
-    print(sales_figures)
+        sales_figures = figure_str.split(',')
+    
+        if validate_figures(sales_figures):
+            print("DATA PROVIDED IS VALID!")
+            break
+    return sales_figures
 
-    # if validate_data(sales_figures):
-       #  print("DATA PROVIDED IS VALID!")
-       #  break
-#return sales_figures
+def validate_figures(values):
+    """
+    inside the try, converts the string values from the data
+    input into integers.
+    Raises ValueError if the string cannot be converted into
+    integers or if there arent enough data values. 
+    when using CEDAS is 7 values exactly.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 7:
+            raise ValueError(
+                f"Exactly 7 values are required. You provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid Data: {e}, please try again.\n")
+        return False
+
+    return True
+
 
 get_sales_figures()
